312b7cc9c3cd13a9b09a6463bc4e7a533363b2c9
turnMeUpSon <milleryan2003@gmail.com>
2023-01-27 15:51:53 +0300

    Updated cv

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,38 +7,6 @@ const Experience = () => {
         <li>
           <i>Higher School of Economics - Bachelor degree in Cybersecurity</i>,
           2018-2022
-          <ul>
-            <li>Graduated with 3.00 GPA</li>
-          </ul>
-        </li>
-        <li>
-          <i>MOBOX</i> - <strong>Blockchain Developer Intern</strong>, 2021-2022
-          <ul>
-            <li>
-              Studied to write smart contracts using Solidity and Ethers.js
-            </li>
-            <li>
-              Studied to test smart contracts using Hardhat and deploy dapps in
-              Goerli testnet
-            </li>
-            <li>
-              Implemented ERC721 smart contarcts and created NFT for farming
-              game
-            </li>
-            <li>Assisted Research &amp; Development on Web3 Products</li>
-          </ul>
-        </li>
-        <li>
-          <i>CoinList</i> - <strong>Solidity Smart Contract Engineer</strong>,
-          2022-2023
-          <ul>
-            <li>
-              Contributed to CoinList staking platform for ERC20 tokens with
-              smart contract development using Solidity, Javascript, TypeScript,
-              Yul and Hardhat
-            </li>
-            <li>Peer reviewed &amp; Audited Solidity code</li>
-          </ul>
         </li>
       </ul>
     </div>
