2f6e0d62603f34b7b50ead200d2a9fa24240170d
Ian Miller <75687988+turnMeUpSon@users.noreply.github.com>
2023-02-27 16:07:36 +0300

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,6 +8,26 @@ const Experience = () => {
           <i>Higher School of Economics - Bachelor degree in Cybersecurity</i>,
           2021-2025
         </li>
+        <li>
+          <i>MOBOX</i> - <strong>Blockchain Developer Intern</strong>, 2021-2022
+          <ul>
+            <li>Studied to write smart contracts using Solidity and Ethers.js</li>
+            <li>Studied to test and deploy smart contracts using Hardhat and Alchemy</li>
+            <li>Implemented ERC721 smart contracts and created NFT for farming game</li>
+            <li>Assisted Research & Development on Web3 Products</li>
+          </ul>
+        </li>
+        <li>
+          <i>MixBytes</i> - <strong>Smart Contract Auditor / DeFi researcher</strong>, 2022-2023
+          <ul>
+            <li>I learned how to work with low-level code and efficient algorithms</li>
+            <li>I deep understood the logic of DeFi and researched web3 protocols</li>
+            <li>Solidity low-level patterns</li>
+            <li>Solidity high-level patterns</li>
+            <li>Audited DeFi protocols such as “Lido protocol” and found vulnerabilities</li>
+            <li>Peer reviewed & Audited Solidity code</li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
